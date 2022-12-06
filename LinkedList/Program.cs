8e8e96c5f4ec93ed61f3879a84969c7891f5a6f4
nikit <nikita.nikita@gmail.com>
2022-12-06 15:37:10 +0530

    Add project files.

---
+++ LinkedList/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //UC1
+            //Console.WriteLine("Hello LinkedList!");
+            //LinkedList list = new LinkedList();
+            //list.Add(56);
+            //list.Add(30);
+            //list.Add(70);
+            //list.Display();
+            //Console.ReadKey();
+
+            //UC2
+            //Console.WriteLine("Hello LinkedList!");
+            //UC2LinkedList List = new UC2LinkedList();
+            //List.add(70);
+            ////list.Display();
+            ////Console.WriteLine("After Insertion Of 30");
+            //List.add(30);
+            ////list.Display();
+            ////Console.WriteLine("After Insertion Of 56");
+            //List.add(56);
+            //List.Display(); 
+
+            //UC3
+            //Console.WriteLine("Hello LinkedList");
+            //UC3LinkedList list = new UC3LinkedList();
+            //list.Append(56);
+            //list.Append(30);
+            //list.Append(70);
+            //list.Display();
+            //Console.ReadKey();
+
+            //UC4
+            //Console.WriteLine("Hello LinkedList!");
+            //UC4LinkedList list = new UC4LinkedList();
+            //list.Add(56);
+            ////list.Add(30);
+            //list.Add(70);
+            //list.InsertAtParticularPosition(2, 30);
+            //list.Display();
+            //Console.ReadKey();
+
+            //UC5
+            //Console.WriteLine("Hello LinkedList");
+            //UC5LinkedList list = new UC5LinkedList();
+            //list.Add(56);
+            //list.Add(30);
+            //list.Add(70);
+            //Console.WriteLine("After Removal of first node then linked list is");
+            //list.RemoveFirstNode();
+            //list.Display();
+            //Console.ReadKey();
+
+            //UC6
+            //Console.WriteLine("Hello LinkedList");
+            //UC6LinkedList list = new UC6LinkedList();
+            //list.Add(56);
+            //list.Add(30);
+            //list.Add(70);
+            //Console.WriteLine("After Removal of Last node then linked list is");
+            //list.RemovaLastNode();
+            //list.Display();
+            //Console.ReadKey();
+
+            //UC7
+            //Console.WriteLine("Hello LinkedList");
+            //UC7LinkedList list = new UC7LinkedList();
+            //list.Add(56);
+            //list.Add(30);
+            //list.Add(70);
+            //list.Search(30);
+            //list.Display();
+            //Console.ReadKey();
+
+            //UC8
+            //Console.WriteLine("Hello LinkedList!");
+            //UC8LinkedList list = new UC8LinkedList();
+            //list.Add(56);
+            //list.Add(30);
+            //list.Add(40);
+            //list.Add(70);
+            //list.Display();
+            //Console.ReadKey();
+
+            //UC9
+            //UC9LinkedList list = new UC9LinkedList();
+            //list.Add(56);
+            //list.Add(30);
+            //list.Add(40);
+            //list.Add(70);
+            //list.Display();
+            //Console.Write("Enter The Element You Have To Delete:");
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Enter The Previous Element Of That Element You Have To Delete:");
+            //int b = Convert.ToInt32(Console.ReadLine());
+            //list.Delete(b, a);
+            //list.Display();
+            //list.size();
+        }
+    }
+}
